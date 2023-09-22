6ad67742fde637872ef1486c78f12638559c0144
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-09-22 12:43:12 +0100

    style: positioning of popup improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,28 @@ export default function Navbar() {
   return (
     <div>
       <div className="w-full h-[40px] flex justify-between items-center px-10 bg-forest text-cream">
-        <ul className="hidden md:flex">
-          <li className="px-4 tracking-wide">
-            <button className="border-2" onClick={() => setShowPopup(true)}>
-              How To Play
-            </button>
-          </li>
-        </ul>
+        <div className="hidden md:flex">
+          <button className="border-2" onClick={() => setShowPopup(true)}>
+            How To Play
+          </button>
+        </div>
       </div>
+
       <HowToPlayPopup trigger={showPopup}>
-        <p>this is the popup</p>
+        <div className=" pt-1 w-2/3 items-center justify-center align-middle">
+          <h3>HOW TO PLAY</h3>
+          <br></br>
+          <p>
+            This game is quite simple: You have 10 attempts to guess each
+            player's ranking. The players are all within the top 20.
+          </p>
+          <p>
+            {" "}
+            Once you have guessed a player, a new player will appear for you to
+            guess.{" "}
+          </p>
+          <p> On guessing all players, the game ends. </p>
+        </div>
       </HowToPlayPopup>
     </div>
   );
--- src/components/howToPlayPopup.js
+++ src/components/howToPlayPopup.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function HowToPlayPopup(props) {
   return props.trigger ? (
-    <div className="fixed bg-cream pt-40">
-      <div className="popup-inner">
-        <button className="close-button ">X</button>
+    <div className="fixed h-1/2 w-full bg-coral flex items-center justify-center align-middle content-center ">
+      <div className="h-full popup-inner flex bg-cream items-center w-2/3  justify-center">
+        <button className="close-button fixed top-5 right-5 ">X</button>
         {props.children}
       </div>
     </div>
