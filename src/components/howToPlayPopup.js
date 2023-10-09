63bee7ea7d23f654922e4e197c8209a3eef0cf7d
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-09 11:04:20 +0100

    refactor: removal of unsued code

--- src/components/howToPlayPopup.js
+++ src/components/howToPlayPopup.js
@@ -51,28 +51,3 @@ function HowToPlayPopup() {
 }
 
 export default HowToPlayPopup;
-
-// function HowToPlayPopup(props) {
-//   return props.trigger ? (
-//     <div className="fixed h-screen w-screen bg-cream z-50">
-//       <div
-//         className="fixed h-1/2 w-full
-//      flex items-center justify-center align-middle content-center "
-//       >
-//         <div className="h-full popup-inner flex bg-blue items-center w-2/3  justify-center">
-//           <button
-//             className="close-button absolute top-0 right-64 h-16 w-16 "
-//             onClick={() => props.setTrigger(false)}
-//           >
-//             close
-//           </button>
-//           {props.children}
-//         </div>
-//       </div>
-//     </div>
-//   ) : (
-//     ""
-//   );
-// }
-
-// export default HowToPlayPopup;
