f44a9bbc7686178c235dbfec4d5caed24de6f213
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-09 10:04:51 +0100

    chore: update of links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Login from "./pages/login";
 import Play from "./pages/play";
