2740d4fe642a94273509adef3f8886edf8ad7d6d
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-09 10:48:19 +0100

    refactor: removal of unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,23 +73,6 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-
-      {/* <HowToPlayPopup trigger={showPopup} setTrigger={setShowPopup}>
-        <div className=" pt-1 w-2/3 items-center justify-center align-middle">
-          <h3>HOW TO PLAY</h3>
-          <br></br>
-          <p>
-            This game is quite simple: You have 10 attempts to guess each
-            player's ranking. Players are all within the top 20.
-          </p>
-          <p>
-            Once you have guessed a player, a new player will appear for you to
-            guess.
-          </p>
-          <p> On guessing all players, the game ends. </p>
-          <p> On running out of guesses, the game ends. </p>
-        </div>
-      </HowToPlayPopup> */}
     </nav>
   );
 }
